f2f21992710aa7cb4ef1047f232bede0718f3cfb
vkensou <boywanyx@163.com>
2020-01-01 13:56:03 +0800

    使用luaide调试

--- Lua/Game/Main.lua
+++ Lua/Game/Main.lua
@@ -1,3 +1,8 @@
+local breakSocketHandle,debugXpCall = require("LuaDebugjit")("localhost",7003)
+local timer = Timer.New(function() 
+breakSocketHandle() end, 1, -1, false)
+timer:Start();
+
 --主入口函数。从这里开始lua逻辑
 function Main()					
 	print("logic start")	 		
